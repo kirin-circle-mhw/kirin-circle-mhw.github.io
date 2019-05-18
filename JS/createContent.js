032d6b2161733193cc1cc4ff42237f70d51179a8
Rumin_ <rumination.ha@gmail.com>
2019-05-18 16:52:33 +0900

    test

--- JS/createContent.js
+++ JS/createContent.js
@@ -1,5 +1,6 @@
 function createCategory() {
     json_data = getJsonFile('/weapondata/weaponlist.json');
+    console.log(json_data);
     category_data = JSON.parse(json_data);
 
     console.log(category_data);
