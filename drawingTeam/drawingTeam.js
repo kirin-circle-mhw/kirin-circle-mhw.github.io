9f8ee3ca9d83ec74eb94135636f4863886a04b6c
Rumin_ <rumination.ha@gmail.com>
2020-03-02 09:58:29 +0900

    add drawingTeam

---
+++ drawingTeam/drawingTeam.js
@@ -0,0 +1,74 @@
+members = [
+    {
+        "name": "루민",
+        "weapon" : "차액, 해머, 태도, 슬액"
+    },
+    {
+        "name": "꾸왕",
+        "weapon": "한손검, 헤보"
+    },
+    {
+        "name": "무반동속사",
+        "weapon": "건랜, 태도, 대검, 슬액"
+    },
+    {
+        "name": "snowlady",
+        "weapon": ""
+    },
+    {
+        "name": "레나",
+        "weapon": ""
+    },
+    {
+        "name": "테이스트오브와일드",
+        "weapon": ""
+    },
+    {
+        "name": "커피물조절장인",
+        "weapon": "랜스, 해머, 슬액"
+    },
+    {
+        "name": "띠용띠용",
+        "weapon": ""
+    },
+    {
+        "name": "진자자",
+        "weapon": ""
+    },
+    {
+        "name": "댈라미",
+        "weapon": ""
+    },
+    {
+        "name": "연휼",
+        "weapon": ""
+    },
+    {
+        "name": "KANTELUV",
+        "weapon": ""
+    },
+    {
+        "name": "혀B",
+        "weapon": "활"
+    },
+    {
+        "name": "PUMPKIN",
+        "weapon": ""
+    },
+    {
+        "name": "ssolid",
+        "weapon": ""
+    },
+    {
+        "name": "톱과젤리",
+        "weapon": ""
+    }
+];
+
+team1_member = [];
+team2_member = [];
+team3_member = [];
+team4_member = [];
+
+wait_member = [];
+
